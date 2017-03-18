469a46433b3aed11f46a363052596d112bbef8b9
octopusprime314 <peter_morley@my.uri.edu>
2017-03-18 12:05:51 -0400

    Add Process Manager
    Quick class additions.

---
+++ BaseCodeCompletelyParallel/ProcessorManager.cpp
@@ -0,0 +1,11 @@
+#include "ProcessorManager.h"
+
+
+ProcessorManager::ProcessorManager(void)
+{
+}
+
+
+ProcessorManager::~ProcessorManager(void)
+{
+}
---
+++ BaseCodeCompletelyParallel/ProcessorManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class ProcessorManager
+{
+public:
+	ProcessorManager(void);
+	~ProcessorManager(void);
+};
+
