150679e54a83c2807a140f9a5541523995ff477f
octopusprime314 <peter_morley@my.uri.edu>
2017-03-18 20:36:11 -0400

    MATLAB File change again....
    not sure why it didn't take the first time

--- PatternDetectiveTCMallocCompletelyParallel/ProcessTimeVsFileSize.m
+++ PatternDetectiveTCMallocCompletelyParallel/ProcessTimeVsFileSize.m
@@ -5,14 +5,32 @@ data3 = csvread('Runs/ThreadsVsThroughput5_37_53222732047.csv');
 figure 
 
 scatter(data(1:end, 1), data(1:end, 2));
-
 xlabel('Processing Time (milliSeconds)');
 ylabel('File size (Bytes)');
 
 figure 
 
-plot(data2(1:end, 1), data2(1:end, 2));
+stem(data2(1:end, 1), data2(1:end, 2));
 
+axis([30 100 0 inf])
 xlabel('Pattern Size');
-ylabel('File Count');        count = 0;
+ylabel('File Count');
+
+%ghetto way of displaying throughput trendlines
+figure 
+prevVal = 0;
+count = 0;
+for idx = 1:numel(data3(1:end, 1))+1
+    element = data3(idx);
+    if(prevVal > element)
+        plot(data3(idx-count:idx-1, 1), data3(idx-count:idx-1, 2));
+        count = 0;
         hold on
+    end
+    prevVal = data3(idx);
+    count = count + 1;
+end
+%plot(data3(1:end, 1), data3(1:end, 2));
+
+xlabel('Threads');
+ylabel('Speedup');
